5279e9e96f8a6ed654f3be2c02f0cd3568faf439
matherzzz <karpoff.matvej@yandex.ru>
2023-10-06 12:11:11 +0500

    the dog turned into a snake

--- hungry_dog.c
+++ hungry_dog.c
@@ -16,6 +16,49 @@ struct dog {
 	int32_t speedY;
 };
 
+struct list {
+	char skin;
+	int32_t x;
+	int32_t y;
+	struct list* next;
+};
+
+struct list* node_create( char skin, int32_t x, int32_t y ) {
+	struct list* node_ptr = malloc(sizeof(struct list));
+	if (node_ptr) {
+		node_ptr->skin = skin;
+	    	node_ptr->x = x;
+	    	node_ptr->y = y;
+        	node_ptr->next = NULL;
+	}
+	return node_ptr;
+}
+
+void list_destroy( struct list* list ) {
+	while (list) {
+		struct list* del = list;
+		list = list -> next;
+		free(del);
+	}
+}
+
+struct list* list_last( struct list * list ) {
+	while (list && list -> next) list = list -> next;
+	return list;
+}
+
+void list_add_back( struct list** old, char skin, int32_t x, int32_t y ) {
+    if (!old) return;
+    if (!(*old)) *old = node_create(skin, x, y);
+    else list_last(*old)->next = node_create(skin, x, y);
+}
+
+void peresch(struct list* list, int32_t x, int32_t y) {
+	if (list->next) peresch(list->next, list->x, list->y);
+	list->x = x;
+	list->y = y;
+}
+
 struct enemy {
 	char skin;
 	int32_t x;
@@ -38,14 +81,18 @@ void print_field(struct playing_field* map) {
 	}
 }
 
+void clear_map(struct playing_field* map) {
+        for (size_t i = 1; i < map->size - 1; i++)
+                fill_str((map->field)[i], ' ', map->size * 2);
+}
+
 void create_field(struct playing_field* map) {
 	map->field = malloc(sizeof(char*) * map->size);
         for (size_t i = 0; i < map->size; i++)
                 (map->field)[i] = malloc(sizeof(char) * map->size * 2);
 	fill_str((map->field)[0], '#', map->size * 2);
-        for (size_t i = 1; i < map->size - 1; i++)
-                fill_str((map->field)[i], ' ', map->size * 2);
-        fill_str((map->field)[map->size - 1], '#', map->size * 2);
+	clear_map(map);
+	fill_str((map->field)[map->size - 1], '#', map->size * 2);
 }
 
 void free_field(struct playing_field* map) {
@@ -54,57 +101,73 @@ void free_field(struct playing_field* map) {
 	free(map->field);
 }
 
-void generate_new_coords(struct enemy* enemy, const struct dog* const dog, size_t size) {
+void generate_new_coords(struct enemy* enemy, const struct playing_field* const map) {
 	do {
-		enemy->x = 1 + rand()%(2 * size - 2);
-		enemy->y = 1 + rand()%(size - 2);
-	} while (dog->x == enemy->x || dog->y == enemy->y);
+		enemy->x = 1 + rand()%(2 * map->size - 2);
+		enemy->y = 1 + rand()%(map->size - 2);
+	} while ((map->field)[enemy->y][enemy->x] != ' ');
 }
 
-bool is_eat(struct enemy* enemy, struct dog* dog) {
-	return enemy->x == dog->x && enemy->y == dog->y;
+bool is_eat(struct enemy* enemy, struct list* list) {
+	return enemy->x == list->x && enemy->y == list->y;
 }
 
-bool is_crash(struct dog* dog, size_t size) {
-	return dog->x == 0 || dog->x == 2*size - 1 || dog->y == 0 || dog->y == size - 1;
+bool is_crash(struct list* list, struct playing_field* map) {
+	return (map->field)[list->y][list->x] != ' ';
+}
+
+void paint_snake(struct playing_field* map, struct list* list) {
+	while(list) {
+		(map->field)[list->y][list->x] = list->skin;
+		list = list->next;
+	}
 }
 
 int main() {
 	char key;
 	int64_t score = 0;
 	int64_t pause = 50;
+	int32_t speedX = 1;
+	int32_t speedY = 0;
 	struct playing_field map = {0};
 	printf("Enter size of playing field: ");
 	scanf("%zu", &(map.size));
 	create_field(&map);
-	struct dog dog = {'@', 5, 5, 1, 0};
+	// struct dog dog = {'@', 5, 5, 1, 0};
+	struct list* snake = node_create('@', 5, 5);
 	struct enemy enemy = {'o', 0, 0, 15};
-	generate_new_coords(&enemy, &dog, map.size);
+	generate_new_coords(&enemy, &map);
 	initscr();
 	do {
 		clear();
-		if (is_crash(&dog, map.size)) break;
-		if (is_eat(&enemy, &dog)) {
+		if (is_eat(&enemy, snake)) {
 			score += enemy.cost;
-			generate_new_coords(&enemy, &dog, map.size);
+			int32_t oldX = enemy.x;
+			int32_t oldY = enemy.y;
+			generate_new_coords(&enemy, &map);
+			map.field[oldY][oldX] = ' ';
+			list_add_back(&snake, snake->skin, snake->x, snake->y );
 		}
-		map.field[dog.y][dog.x] = dog.skin;
+		if (is_crash(snake, &map)) break;
+		clear_map(&map);
+		paint_snake(&map, snake);
 		map.field[enemy.y][enemy.x] = enemy.skin;
 		printw("score: %"PRId64"\n", score);
 		print_field(&map);
 		timeout(0);
 		key = getch();
 		noecho();
-		map.field[dog.y][dog.x] = ' ';
-		if (key == 'w') { dog.speedY = -1; dog.speedX = 0; pause = 100; }
-		if (key == 's') { dog.speedY = 1; dog.speedX = 0; pause = 100; }
-		if (key == 'a') { dog.speedX = -1; dog.speedY = 0; pause = 50; }
-		if (key == 'd') { dog.speedX = 1; dog.speedY = 0; pause = 50; }
-		dog.x += dog.speedX;
-		dog.y += dog.speedY;
+		if (key == 'w') { speedY = -1; speedX = 0; pause = 100; }
+		if (key == 's') { speedY = 1; speedX = 0; pause = 100; }
+		if (key == 'a') { speedX = -1; speedY = 0; pause = 50; }
+		if (key == 'd') { speedX = 1; speedY = 0; pause = 50; }
+		peresch(snake, snake->x + speedX, snake->y + speedY);
+		//dog.x += dog.speedX;
+		//dog.y += dog.speedY;
 		napms(pause);
 	} while (key != 'e');
 	endwin();
+	list_destroy(snake);
 	free_field(&map);
 	return 0;
 }
